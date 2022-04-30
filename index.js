17acb5d049f50e19e979fcb7d35bc6a5fbef7df9
allibis <andrix1250@gmail.com>
2022-04-30 17:08:49 +0200

    implemented normalization

--- index.js
+++ index.js
@@ -1,32 +1,47 @@
 
 // object that holds the NN settings
 let options = {
-  layers: [2, 5, 1],
+  layers: [3, 5, 10, 11],
+  output_labels: ["black", "white", "gray", "red", "blue", "orange", "purple", "green", "yellow", "brown", "pink"],
   learningRate: 0.5,
-  epochs: 200,
+  epochs: 10,
   debug: false,
-  activation: "sigmoid"
+  activation: "sigmoid",
+  max_input: 255,
+  min_input: 0,
+  normalize_data: true
 };
 
+// white, black, gray, red, blue, orange, purple, green, yellow, brown, pink
 let nn;
-let data = [
-  {
-    inputs: [1, 1],
-    target: [0]
-  },
-  {
-    inputs: [0, 0],
-    target: [0]
-  },
-  {
-    inputs: [0, 1],
-    target: [1]
-  },
-  {
-    inputs: [1, 0],
-    target: [1]
-  }
-];
+let dataset = [];
+let jsonObj;
+// fetch("dataset.json")
+//   .then(response => response.json())
+//   .then(json => {dataset = json});
+
+function preload() {
+  jsonObj = loadJSON("dataset.json");
+}
+
+// let data = [
+//   {
+//     inputs: [1, 1],
+//     target: [0]
+//   },
+//   {
+//     inputs: [0, 0],
+//     target: [0]
+//   },
+//   {
+//     inputs: [0, 1],
+//     target: [1]
+//   },
+//   {
+//     inputs: [1, 0],
+//     target: [1]
+//   }
+// ];
 
 // let data = [
 //   {
@@ -48,11 +63,11 @@ let data = [
 // ];
 
 // let data = [
-  // {
-  //   first: 1,
-  //   second: 1,
-  //   target: [0]
-  // },
+//   {
+//     first: 1,
+//     second: 1,
+//     target: [0]
+//   },
 //   {
 //     first: 0,
 //     second: 0,
@@ -70,28 +85,37 @@ let data = [
 //   }
 // ];
 
-let dataset;
 function setup() {
   noCanvas();
 
-  nn = new NeuralNetwork(options);
-
-  dataset = [];
-  for (let i = 0; i < 100; i++) {
-    let info = data[Math.floor(Math.random() * data.length)];
-    dataset.push(info);
+  for (let elem in jsonObj){
+    dataset.push(jsonObj[elem]);
   }
 
-  const trainset = dataset.slice(0, 50);
-  const testset = dataset.slice(50, 100);
+  nn = new NeuralNetwork(options);
+  console.log(dataset);
+  console.log(nn.normalize(dataset));
+  // dataset = [];
+  // for (let i = 0; i < 100; i++) {
+  //   let info = data[Math.floor(Math.random() * data.length)];
+  //   dataset.push(info);
+  // }
+
+  // divides the dataset in a training part and a testing part
+  let trainSlice = Math.floor(dataset.length * 0.7);
+
+  const trainset = dataset.slice(0, trainSlice);
+  const testset = dataset.slice(trainSlice, dataset.length);
 
   let accuracy = nn.test(testset);
-  console.log("R² = " + accuracy);
-  // console.log("accuracy before = " + accuracy + "%");
+  accuracy = Math.round((accuracy + Number.EPSILON) * 100) / 100;
+  console.log(`accuracy:${accuracy}%`);
 
   console.log("training...");
   nn.train(trainset);
 
   accuracy = nn.test(testset);
-  console.log("R² = " + accuracy);
+  accuracy = Math.round((accuracy + Number.EPSILON) * 100) / 100;
+  console.log(`accuracy:${accuracy}%`);
+
 }
